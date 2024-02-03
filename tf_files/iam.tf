b0d216a64f64fa786d75771752e93560029ab881
Albert WSL <dewatrigant.albert@gmail.com>
2024-02-03 11:21:20 +0100

    create terraform job

---
+++ tf_files/iam.tf
@@ -0,0 +1,6 @@
+resource "google_project_iam_custom_role" "impersonification_role" {
+  role_id     = "impersonification_role"
+  title       = "Impersonification Role"
+  description = "Role to impersonate Service account"
+  permissions = ["iam.serviceaccounts.actAs"]
+}
