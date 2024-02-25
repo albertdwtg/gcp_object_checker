26db3cdd2ae57b9bfe4019b393cc0bd19596058b
Albert WSL <dewatrigant.albert@gmail.com>
2024-02-25 16:44:43 +0100

    create cr jobs

--- tf_files/cloud_run.tf
+++ tf_files/cloud_run.tf
@@ -1,29 +1,43 @@
-resource "google_service_account" "sa-job-obj-checker" {
-  account_id   = "sa-obj-checker-job"
-  display_name = "Object Checker job service account"
-}
-
+# resource "google_cloud_run_v2_job" "object_checker" {
+#   name     = var.project_id
+#   location = var.region
+#   template {
+#     template {
+#       containers {
+#         image = "${var.artifact_registry_image_name}:latest"
+#         resources {
+#           limits = {
+#             cpu    = var.max_cpu_job
+#             memory = var.max_memory_job
+#           }
+#         }
+#       }
+#       service_account = google_service_account.sa-job-obj-checker.email
+#     }
+#   }
+#   lifecycle {
+#     ignore_changes = [
+#       launch_stage,
+#     ]
+#   }
+# }
 
 resource "google_cloud_run_v2_job" "object_checker" {
-  name     = var.project_id
+  for_each = locals.jobs
+  name     = each.key
   location = var.region
   template {
     template {
       containers {
-        image = "${var.artifact_registry_image_name}:latest"
+        image = "europe-west1-docker.pkg.dev/${var.project_id}/jobs-launcher/${each.key}:latest"
         resources {
           limits = {
-            cpu    = var.max_cpu_job
-            memory = var.max_memory_job
+            cpu    = each.max_cpu_job
+            memory = each.max_memory_job
           }
         }
       }
       service_account = google_service_account.sa-job-obj-checker.email
     }
-  }
-  lifecycle {
-    ignore_changes = [
-      launch_stage,
-    ]
   }
 }
---
+++ tf_files/locals.tf
@@ -0,0 +1,8 @@
+locals {
+    jobs = {
+        first_job = {
+            max_cpu_job = 1
+            max_memory_job = "512Mi"
+        }
+    }
+}
