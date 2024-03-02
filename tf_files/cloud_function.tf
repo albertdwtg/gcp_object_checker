59e0eb4f002cd3e065ce1ce08496d2a38f9a6c2d
Albert WSL <dewatrigant.albert@gmail.com>
2024-03-02 18:35:47 +0100

    create cloud function

---
+++ tf_files/cloud_function.tf
@@ -0,0 +1,12 @@
+resource "google_cloudfunctions_function" "jobs_launcher_cf" {
+  name        = "jobs_launcher"
+  description = "Jobs launcher cloud function"
+  runtime     = "python312"
+
+  available_memory_mb          = 128
+  source_archive_bucket        = google_storage_bucket.jobs_launcher_bucket.name
+  source_archive_object        = google_storage_bucket_object.blob_cf_code.name
+  trigger_http                 = true
+  timeout                      = 60
+  entry_point                  = "run"
+}
---
+++ tf_files/storage.tf
@@ -0,0 +1,14 @@
+resource "google_storage_bucket" "jobs_launcher_bucket" {
+  name     = "jobs_launcher_bucket"
+  location = "EU"
+}
+data "archive_file" "zip_cf_code" {
+  type        = "zip"
+  output_path = "/tmp/function-source.zip"
+  source_dir  = "cloud_function_code/"
+}
+resource "google_storage_bucket_object" "blob_cf_code" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.jobs_launcher_bucket.name
+  source = data.archive_file.zip_cf_code.output_path 
+}
