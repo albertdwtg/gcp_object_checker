432ec52596c7cc7828739450f7d8e6ee03ec25d7
Albert WSL <dewatrigant.albert@gmail.com>
2024-03-02 13:54:20 +0100

    move to cr service

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,3 +1,16 @@
+import os
+
+from flask import Flask
 from main import run
 
-print(run())
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    name = os.environ.get("NAME", "World")
+    print(run())
+    return f"Hello {name}!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- tf_files/cloud_run.tf
+++ tf_files/cloud_run.tf
@@ -3,48 +3,21 @@ resource "google_service_account" "sa-jobs-launcher" {
   display_name = "Jobs launcher job service account"
 }
 
-# resource "google_cloud_run_v2_job" "object_checker" {
-#   name     = var.project_id
-#   location = var.region
-#   template {
-#     template {
-#       containers {
-#         image = "${var.artifact_registry_image_name}:latest"
-#         resources {
-#           limits = {
-#             cpu    = var.max_cpu_job
-#             memory = var.max_memory_job
-#           }
-#         }
-#       }
-#       service_account = google_service_account.sa-job-obj-checker.email
-#     }
-#   }
-#   lifecycle {
-#     ignore_changes = [
-#       launch_stage,
-#     ]
-#   }
-# }
-
-resource "google_cloud_run_v2_job" "object_checker" {
+resource "google_cloud_run_v2_service" "object_checker" {
   for_each = local.jobs
   name     = each.key
   location = var.region
   template {
-    template {
-      timeout = each.value.timeout
-      max_retries = each.value.max_retries
-      containers {
-        image = "europe-west1-docker.pkg.dev/${var.project_id}/jobs-launcher/${each.key}:${var.images_tag}"
-        resources {
-          limits = {
-            cpu    = each.value.max_cpu_job
-            memory = each.value.max_memory_job
-          }
+    timeout = each.value.timeout
+    containers {
+      image = "europe-west1-docker.pkg.dev/${var.project_id}/jobs-launcher/${each.key}:${var.images_tag}"
+      resources {
+        limits = {
+          cpu    = each.value.max_cpu_job
+          memory = each.value.max_memory_job
         }
       }
-      service_account = google_service_account.sa-jobs-launcher.email
     }
+    service_account = google_service_account.sa-jobs-launcher.email
   }
 }
