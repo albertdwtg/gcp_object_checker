8c38b83cf3229df1a652a770b16e69fded42e783
Albert WSL <dewatrigant.albert@gmail.com>
2024-05-09 15:35:05 +0200

    first request to cloud run

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -3,18 +3,26 @@ import os
 from flask import Flask
 from main import run
 from flask import request
+import json
 
 container_port = os.environ.get("CONTAINER_PORT")
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
-def hello_world():
-    """Example Hello World route."""
+def entrypoint():
     name = os.environ.get("NAME", "World")
     print(request.data)
+    input_variables = request.data.decode()
+    input_variables = json.loads(input_variables)
+    load_env_variables(input_variables = input_variables)
     print(run())
     return f"Hello {name}!"
 
+
+def load_env_variables(input_variables: dict) -> None:
+    for k, v in input_variables.items():
+        os.environ[k] = v
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=container_port)
+    app.run(debug=False, host="0.0.0.0", port=container_port)
--- src/first-job/main.py
+++ src/first-job/main.py
@@ -1,2 +1,5 @@
+import os
+
 def run():
     print("image run 1")
+    print(os.environ.get("var1"))
