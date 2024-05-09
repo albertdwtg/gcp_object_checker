76172c5fd80028cf6de0ee3ef7f8826d502a4d09
Albert WSL <dewatrigant.albert@gmail.com>
2024-05-09 15:13:03 +0200

    first request to cloud run

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -2,15 +2,21 @@ import os
 
 from flask import Flask
 from main import run
+from flask import request
+
+container_port = os.environ.get("CONTAINER_PORT")
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def hello_world():
     """Example Hello World route."""
     name = os.environ.get("NAME", "World")
+    print(request)
+    print(request.headers)
+    print(request.data)
     print(run())
     return f"Hello {name}!"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=container_port)
