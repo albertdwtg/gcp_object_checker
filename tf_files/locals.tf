55f14340c9096d825a22ee62028354fb415ab133
Albert WSL <dewatrigant.albert@gmail.com>
2024-05-11 14:51:07 +0200

    remove lookups

--- tf_files/locals.tf
+++ tf_files/locals.tf
@@ -2,10 +2,10 @@ locals {
   topics_paths = jsonencode({ for k, v in local.jobs : k => google_pubsub_topic.jobs_launcher_topics[k].id })
   jobs = {
     first-job = {
-      # max_cpu_job    = 1
-      # max_memory_job = "512Mi"
-      # timeout        = "15s"
-      # max_retries    = 2
+      max_cpu_job    = 1
+      max_memory_job = "512Mi"
+      timeout        = "15s"
+      max_retries    = 2
       scheduler = {
         description      = "First job scheduler"
         cron             = "*/30 * * * *"
