9f13044f217bee0c8b22b869dd436db08c17c889
Albert WSL <dewatrigant.albert@gmail.com>
2024-05-25 13:49:18 +0200

    new project for air quality

--- tf_files/main.tf
+++ tf_files/main.tf
@@ -19,4 +19,5 @@ module "air_quality" {
   project_id              = var.project_id
   cloud-run-sa-email      = google_service_account.sa-jobs-launcher.email
   open_weather_api_secret = var.open_weather_api_secret
+  depends_on = [ google_project_service.gcp_apis ]
 }
