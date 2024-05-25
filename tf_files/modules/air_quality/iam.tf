26132cfb1c18ce754768f41d2424f8173ec9856e
Albert WSL <dewatrigant.albert@gmail.com>
2024-05-25 15:45:09 +0200

    modify python code for bq

--- tf_files/modules/air_quality/iam.tf
+++ tf_files/modules/air_quality/iam.tf
@@ -3,3 +3,9 @@ resource "google_bigquery_dataset_iam_member" "editor" {
   role       = "roles/bigquery.dataEditor"
   member     = "serviceAccount:${var.cloud-run-sa-email}"
 }
+
+resource "google_bigquery_dataset_iam_member" "user" {
+  dataset_id = google_bigquery_dataset.raw_data_dataset.dataset_id
+  role       = "roles/bigquery.user"
+  member     = "serviceAccount:${var.cloud-run-sa-email}"
+}
