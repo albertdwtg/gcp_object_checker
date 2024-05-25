22f0e3c451d95e754d93604a4b485934c94be4f7
Albert WSL <dewatrigant.albert@gmail.com>
2024-05-25 17:33:53 +0200

    modify python code for bq

--- src/first-job/main.py
+++ src/first-job/main.py
@@ -1,6 +1,7 @@
 import time
 from open_weather import Client
 from google.cloud import secretmanager
+from datetime import datetime, timedelta
 
 
 cities = {
@@ -18,14 +19,23 @@ def run(**kwargs):
     # print(kwargs)
     
     c = Client("b964662692dfe4505387dd88765771bf")
+    report_type = kwargs.get("report_type")
+    if report_type.upper()=="CATCHUP":
+        start_time = kwargs["start_time"]
+        end_time = kwargs["end_time"]
+    else:
+        report_type = "DAILY"
+        start_time = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+        end_time = datetime.today().strftime('%Y-%m-%d')
+    
     for city, coord in cities.items():
         print(city)
         c.run_report(
-            report_type="Daily",
+            report_type=report_type,
             latitude=coord[0],
             longitude=coord[1],
-            start_time_str = "2023-05-25",
-            end_time_str = "2024-05-25",
+            start_time_str = start_time,
+            end_time_str = end_time,
             table_name = city.lower(),
             project_id = kwargs["project_id"],
             dataset_id = kwargs["dataset_id"]
